6a0dc95cfbaf388a4d044c4ac0c8fb8acda2c501
tstevens003 <tstevens003@PHS-B22B-20.cte.hampton.k12.va.us>
2014-01-14 18:58:54 -0500

    inserted PID and Joystick and started autonomous.

--- src/com/phantommentalists/Twenty14/AerialAssist.java
+++ src/com/phantommentalists/Twenty14/AerialAssist.java
@@ -7,25 +7,57 @@
 package com.phantommentalists.Twenty14;
 
 import edu.wpi.first.wpilibj.SimpleRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.CANTimeoutException;
 
+/*
+ */
 /**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the SimpleRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
+ *
+ * @author tstevens003
  */
 public class AerialAssist extends SimpleRobot {
-
-    /**
-     * This function is called once each time the robot enters autonomous mode.
-     */
+    
+    public PIDController aimController;
+    public PIDController turnController;
+    protected Joystick driveStick;
+    protected Joystick shooterStick;
+    protected Joystick armStick;
+    DriverStation ds;
+    boolean turning = false;
+    public AerialAssist(){
+        try {
+            drive = new CrabDrive();
+            launcher = new LauncherSystem();
+            //gamemech = New GameMech();
+        } catch (CANTimeoutException e) {
+            
+        }
+    }
     public void autonomous() {
+        try {
+            double _P = (ds.getAnalogIn(1) / 3.3) * 500.0;
+            double _I = (ds.getAnalogIn(2) / 3.3);
+            double _D = (ds.getAnalogIn(3) / 3.3);
+            double kDamp = (ds.getAnalogIn(1) / 3.3) * 50;
+            System.out.println("P: " + _P + ", I: " + _I + ", D: " + _D);
+            aimController.setPID(_P, _I, _D);
+            if (turnController != null) {
+                turnController.setPID(_P, _I, _D);
+            }
+            RobotState state = new RobotState();
+            while (isAutonomous() &&isEnabled()) {
+                dash.updateDashboard();
+                double time = Timer.getFPGATimestamp();
+                switch (state.getState()) {
+                    
+                }
+            }
     }
-
-    /**
-     * This function is called once each time the robot enters operator control.
-     */
     public void operatorControl() {
+        
     }
-}
+    }
