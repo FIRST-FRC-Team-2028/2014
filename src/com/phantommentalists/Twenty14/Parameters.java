e9572be09230df6c9e97ff3c93290a3341c5c423
tstevens003 <tstevens003@PHS-B22B-20.cte.hampton.k12.va.us>
2014-01-15 16:32:21 -0500

    inserted kJoyStickDeadband and 4 in/out solenoid Channels

--- src/com/phantommentalists/Twenty14/Parameters.java
+++ src/com/phantommentalists/Twenty14/Parameters.java
@@ -15,4 +15,15 @@ public class Parameters {
     public static final int steeringIntegralValue = -1;
     public static final int steeringDerivativeValue = -1;
     public static final double maxMotorVoltage = 12.0;
+    public final static int oneInSolenoidChannel = -1;
+    public final static int oneOutsolenoidChannel = -1;
+    public final static int twoInSolenoidChannel = -1;
+    public final static int twoOutsolenoidChannel = -1;
+    public final static int threeInSolenoidChannel = -1;
+    public final static int threeOutsolenoidChannel = -1;
+    public final static int fourInSolenoidChannel = -1;
+    public final static int fourOutsolenoidChannel = -1;
+    public static final double kJoyStickDeadband = 0.05;
+    
+    
 }
