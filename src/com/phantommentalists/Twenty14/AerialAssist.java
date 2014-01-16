05b7ca6051e6dacee914eb2b14e20604f9e3a0de
tstevens003 <tstevens003@PHS-B22B-20.cte.hampton.k12.va.us>
2014-01-16 18:55:44 -0500

    Gear toggle buttons and all that is left is using other classes such as drive, catcher ect. and putting it in. :-)

--- src/com/phantommentalists/Twenty14/AerialAssist.java
+++ src/com/phantommentalists/Twenty14/AerialAssist.java
@@ -8,11 +8,10 @@ package com.phantommentalists.Twenty14;
 
 import edu.wpi.first.wpilibj.SimpleRobot;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.CANTimeoutException;
-
+import edu.wpi.first.wpilibj.PIDController;
 /*
  */
 /**
@@ -28,6 +27,8 @@ public class AerialAssist extends SimpleRobot {
     protected Joystick armStick;
     DriverStation ds;
     boolean turning = false;
+    private final LauncherSystem launcher;
+    private final CrabDrive drive;
     public AerialAssist(){
         try {
             drive = new CrabDrive();
@@ -49,15 +50,70 @@ public class AerialAssist extends SimpleRobot {
                 turnController.setPID(_P, _I, _D);
             }
             RobotState state = new RobotState();
-            while (isAutonomous() &&isEnabled()) {
+            while (isAutonomous() && isEnabled()) {
                 dash.updateDashboard();
                 double time = Timer.getFPGATimestamp();
                 switch (state.getState()) {
-                    
+                    case RobotState.drive:
+                       drive.drive(Parameters.autonomous_DRIVE_FORWARD_SPEED, 0.0,KDamp);
+                        if (Timer.getFPGATimestamp() - time < 0.5) {
+                            state.nextState();
                 }
             }
     }
-    public void operatorControl() {
-        
-    }
-    }
+    public void operatorControl(String currentGear) {
+         double kDamp = (ds.getAnalogIn(1) / 3.3) * 50;
+//        double _P = (ds.getAnalogIn(1) / 3.3) * 100.0;
+//        double _I = (ds.getAnalogIn(2) / 3.3) * 0.01;
+//        double _D = (ds.getAnalogIn(3) / 3.3) * 0.01;
+//        aimController.setPID(_P, _I, _D);
+//        if (turnController != null)
+//        {
+//            turnController.setPID(_P, _I, _D);
+//        }
+        int i = 0;
+        while (isOperatorControl() && isEnabled()) {
+//            dash.updateDashboard();
+            //
+            // Driver Controls
+            //
+           try {
+                double drivePercent = driveStick.getY() * -1.0;
+                if (drivePercent < Parameters.kJoystickDeadband
+                        && drivePercent > (-1.0 * Parameters.kJoystickDeadband)) {
+                    drivePercent = 0.0;
+                }
+                double turnPercent = driveStick.getX();
+                if (turnPercent < Parameters.kJoystickDeadband
+                        && turnPercent > (-1.0 * Parameters.kJoystickDeadband)) {
+                    turnPercent = 0.0;
+                }
+                switch (i) {
+                    default:
+                        i++;
+                        break;
+                    case 3:
+                        System.out.println("Drive value: " + drivePercent + ", Turn value: " + turnPercent);
+                        i = 0;
+                        break;
+                }
+                drive.drive(drivePercent, turnPercent, kDamp);
+            } catch (CANTimeoutException e) {
+                System.out.println(e);
+            }
+
+            boolean lowGear = driveStick.getRawButton(Parameters.kLowGearButton);
+            boolean highGear = driveStick.getRawButton(Parameters.kHighGearButton);
+            Tread.Gear presentGear = drive.getGear();
+            Tread.Gear newGear = presentGear;
+            if (highGear) {
+                newGear = Tread.Gear.kHigh;
+                currentGear = "High Gear";
+            }
+            if (lowGear) {
+                newGear = Tread.Gear.kLow;
+                currentGear = "Low Gear";
+            }
+            if (presentGear != newGear) {
+                drive.setGear(newGear);
+            }
