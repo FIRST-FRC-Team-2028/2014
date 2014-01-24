aa83c51c695fc14a2b8d576d75ac888607d848cf
mburt001 <mburt001@PHS-B22B-17.cte.hampton.k12.va.us>
2014-01-24 11:33:39 -0500

    using fixed wheel from testdrive branch

--- src/com/phantommentalists/Twenty14/Wheel.java
+++ src/com/phantommentalists/Twenty14/Wheel.java
@@ -1,113 +1,65 @@
 package com.phantommentalists.Twenty14;
-
 /*
- * 
- * and open the template in the editor.   
- * 
- * 
- * 
- * (added comment)
- * 
- * 
- * 
- * 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
  */
+
 import edu.wpi.first.wpilibj.CANJaguar;
 import edu.wpi.first.wpilibj.can.CANTimeoutException;
 
 /**
  *
  * @author mburt001
- *
  */
 public class Wheel {
 
     CANJaguar steeringMotor;
-    private double setPoint = 0.0;
+    CANJaguar driveMotor;
+    private double setPoint;
+    private boolean driving = false;
     private boolean steering = false;
 
-    /**
-     *
-     * @param steerID
-     * @throws CANTimeoutException
-     *
-     */
     public Wheel(int steerID) throws CANTimeoutException {
-
-        steeringMotor = new CANJaguar(steerID, CANJaguar.ControlMode.kPosition);
-        steeringMotor.setPositionReference(CANJaguar.PositionReference.kPotentiometer);
+        steeringMotor = new CANJaguar(steerID, CANJaguar.ControlMode.kPercentVbus);
         steeringMotor.configMaxOutputVoltage(Parameters.maxMotorVoltage);
         steeringMotor.configNeutralMode(CANJaguar.NeutralMode.kBrake);
         steeringMotor.setPID(Parameters.steeringProportionalValue,
                 Parameters.steeringIntegralValue,
                 Parameters.steeringDerivativeValue);
         steering = true;
+
     }
 
-    /**
-     *
-     * @throws CANTimeoutException
-     *
-     */
     public void enablePositionControl() throws CANTimeoutException {
         if (steering) {
             steeringMotor.enableControl();
         }
     }
 
-    /**
-     *
-     * @throws CANTimeoutException
-     *
-     */
     public void disablePositionControl() throws CANTimeoutException {
         if (steering) {
             steeringMotor.disableControl();
         }
     }
 
-    /**
-     *
-     * @param outputValue
-     * @return
-     *
-     */
-    public double convertJoystickToPosition(double outputValue) {
-        double scaled = (setPoint + 1) / 2;
-//      double scaled = ((outputValue * 0.26) + 0.55);
-        return scaled;
-    }
-
-    /**
-     *
-     * @param joystickPercentPower
-     * @return
-     * @throws CANTimeoutException
-     *
-     */
-    public boolean setPosition(double joystickPercentPower) throws CANTimeoutException {
-        if (steering) {
-            if (joystickPercentPower == setPoint) {
-                return false;
-            }
-            setPoint = joystickPercentPower;
-            double outputValue = convertJoystickToPosition(joystickPercentPower);
-            steeringMotor.setX(outputValue);
+    public boolean setPosition(double outputValue) throws CANTimeoutException {
+        steeringMotor.setX(outputValue);
+        if (steeringMotor.getPosition() == outputValue) {
             return true;
         }
         return false;
     }
 
-    /**
-     *
-     * @return @throws CANTimeoutException
-     *
-     */
     public double getPosition() throws CANTimeoutException {
         if (steering) {
             return steeringMotor.getPosition();
         }
         return -1;
+    }
 
+    public void setSpeed(double outputValue) throws CANTimeoutException {
+        if (driving) {
+            driveMotor.setX(outputValue);
+        }
     }
 }
