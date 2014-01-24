2d063b6986d4ec4938616710868ef49d4f689e3f
mburt001 <mburt001@PHS-B22B-17.cte.hampton.k12.va.us>
2014-01-24 08:57:42 -0500

    changed solenoids to relay left and relay right

--- src/com/phantommentalists/Twenty14/ChopSticks.java
+++ src/com/phantommentalists/Twenty14/ChopSticks.java
@@ -1,53 +1,56 @@
 package com.phantommentalists.Twenty14;
 
 /**
- * 
- *  and open the template in the editor.
- * 
+ *
+ * and open the template in the editor.
+ *
  */
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.CANJaguar;
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.Victor;
 
 /**
- * 
+ *
  * ChopSticks allocation
+ *
  * @author mburt001
- * 
+ *
  */
 public class ChopSticks {
 
-    public Solenoid left;
+    public Relay left;
     public CANJaguar rotationMotor;
-    public Solenoid right;
+    public Relay right;
 
     /**
-     * 
-     * 
-     * 
+     *
+     *
+     *
      */
     public void deploy() {
     }
 
     /**
-     * 
-     * 
-     * 
+     *
+     *
+     *
      */
     public void retract() {
     }
 
     /**
-     * 
-     * 
-     * 
+     *
+     *
+     *
      */
     public void getPosition() {
     }
 
     /**
-     * 
-     * 
-     * 
+     *
+     *
+     *
      */
     public void groundPass() {
     }
