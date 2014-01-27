73ef3eeba555405c4bac1d6dd93c6c40592a9023
mburt001 <mburt001@PHS-B22B-17.cte.hampton.k12.va.us>
2014-01-27 14:56:44 -0500

    added comment

--- src/com/phantommentalists/Twenty14/ChopSticks.java
+++ src/com/phantommentalists/Twenty14/ChopSticks.java
@@ -20,43 +20,54 @@ import edu.wpi.first.wpilibj.Victor;
 public class ChopSticks {
 
     public Relay left;
-    public Victor rotationMotor;
     public Relay right;
 
     /**
-     *
      * ChopSticks()
      *
      * not finished public ChopSticks
-     *
      */
     public ChopSticks(int leftArmChannel, int rightArmChannel, int rotationChannel) {
         left = new Relay(leftArmChannel);
         right = new Relay(rightArmChannel);
-        rotationMotor = new Victor(rotationChannel);
+    
     }
-
+    /**
+     * 
+     * turnOnChopSticks()
+     * 
+     * This method turns both left and right ChopSticks on.
+     */
     public void turnOnChopSticks() {
         left.set(Relay.Value.kForward);
         right.set(Relay.Value.kReverse);
     }
-    
+     /**
+     * turnOffChopSticks()
+     * 
+     * This method turns both left and right ChopSticks off.
+     */
     public void turnOffChopSticks() {
        left.set(Relay.Value.kOff);
        right.set(Relay.Value.kOff);
     }
     
-    public void setRotation(double outputValue) {
-        rotationMotor.set(outputValue);
-    }
-    
+     /**
+     * isChopSticksOn()
+     *                                                                                                                                                                                                                                                                                                                                                                                                                                  
+     * This method returns whether or not the ChopSticks are on or off, rotating or stationary.
+     */
     public boolean isChopSticksOn() {
         if (left.get() != Relay.Value.kOff && right.get() != Relay.Value.kOff) {
          return true;
         }
         return false;
     }
-    
+     /**
+     * isChopSticksOff()
+     * 
+     * This method returns whether or not the ChopSticks are on or off, rotating or stationary.
+     */
     public boolean isChopSticksOff() {
         if (left.get() == Relay.Value.kOff && right.get() == Relay.Value.kOff) {
             return true;
