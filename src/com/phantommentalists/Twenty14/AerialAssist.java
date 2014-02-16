964e08cf0daf18847930b6dde412f50fdc9ecf91
Justin <Justin@Laptop>
2014-02-16 14:32:23 -0500

    Fixed it so that the reference is the joystick for if the crabValue should be used (as it is sometimes outside the deadband if the joystick is at 0, meaning that it reads a large angle and would try to go to it)

--- src/com/phantommentalists/Twenty14/AerialAssist.java
+++ src/com/phantommentalists/Twenty14/AerialAssist.java
@@ -1,4 +1,4 @@
-/*----------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/ 
 /* Copyright (c) FIRST 2008. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
@@ -15,80 +15,89 @@ import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.Compressor;
 /*
  */
-
 /**
  *
  * @author mburt001
  */
-public class AerialAssist extends SimpleRobot {
+public class AerialAssist extends SimpleRobot
+{
 
     public PIDController aimController;
     public PIDController turnController;
     protected Joystick driveStick;
     public CrabDrive drive;
     public GameMech gameMech;
-    public Compressor compressor;
+    public Compressor  compressor;
     public DriverStation ds;
 
-    public AerialAssist() {
+    public AerialAssist()
+    {
         driveStick = new Joystick(1);
-        try {
+        try
+        {
             drive = new CrabDrive();
             gameMech = null;
             compressor = new Compressor(Parameters.compressorSwitchChannel,
                     Parameters.compressorRelayChannel);
             ds = DriverStation.getInstance();
-        } catch (CANTimeoutException ex) {
+        } catch (CANTimeoutException ex)
+        {
             ex.printStackTrace();
         }
     }
 
-    public void autonomous() {
+    public void autonomous()
+    {
         int value;
-        final int kDriving = 0;
-        final int kShooting = 1;
-        final int kWaitingForHot = 2;
+        final int kDriving =0;
+        final int kShooting =1;
+        final int kWaitingForHot =2;
         value = kWaitingForHot;
     }
 
-    public void operatorControl() {
+    public void operatorControl()
+    {
         int count = 0;
-
-        try {
+       
+        try
+        {
             drive.enablePositionControl();
-            while (isEnabled() && isOperatorControl()) {
-                double driveValue = FRCMath.getPolarMagnitude(driveStick.getX(),
-                        driveStick.getY());
-                double turnValue = (((ds.getAnalogIn(1) / 3.3) * 2) - 1);
-                double crabValue = FRCMath.convertDegreesToJoystick(FRCMath.getPolarAngle(driveStick.getX(),
-                        driveStick.getY()));
-                if (driveStick.getTrigger()) {
-                    if (turnValue > 0.05 || turnValue < -0.05) {
-                        drive.turnOnAxis(turnValue * 2);
-                    }
-                } else if (turnValue > 0.05 || turnValue < -0.05) {
-                    drive.slewDrive(driveValue, turnValue / 2);
-                } else {
-                    if (crabValue > 0.05 || crabValue < -0.05) {
-                        drive.crabDrive(driveValue, turnValue);
-                    } else {
-                        drive.crabDrive(driveValue, 0); //3.14159265358979323846264338327950
+            while (isEnabled() && isOperatorControl())
+            {
+                double driveValue = driveStick.getMagnitude();
+                double turnValue = (((ds.getAnalogIn(1)/3.3)*2)-1) * 2;
+                double crabValue = FRCMath.convertDegreesToJoystick(driveStick.getDirectionDegrees());
+                if (driveStick.getX() > 0.05 || driveStick.getX() < -0.05)  
+                {
+                    drive.crabDrive(driveValue, crabValue);
+                } 
+                else
+                {
+                    if (turnValue > 0.05 || turnValue < -0.05)
+                    {
+                        drive.slewDrive(driveValue, turnValue);
+                    } else
+                    {
+                        drive.slewDrive(driveValue, 0); //3.14159265358979323846264338327950
                     }
                 }
                 count++;
-                if (count % 5 == 0) {
+                if (count % 5 == 0)
+                {
                     count = 0;
                     System.out.println("Degrees : " + crabValue);
-                    drive.printTelemetry();
+                    drive.printTelemetry(); 
                 }
                 Timer.delay(Parameters.TIMER_DELAY);
                 drive.processCrabDrive();
-                if (gameMech != null) {
+                if (gameMech != null)
+                {
                     gameMech.processGameMech();
                 }
             }
             drive.disablePositionControl();
-        } catch (CANTimeoutException ex) {
+        } catch (CANTimeoutException ex)
+        {
             ex.printStackTrace();
         }
     }
